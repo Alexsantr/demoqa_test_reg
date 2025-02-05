59c0dd37e3bc25a01530e4bb7e732068f5a3db6c
Trek119 <kopen_kin@mail.ru>
2025-02-05 19:46:49 +0300

    update code

--- src/test/java/tests/TestSetting.java
+++ src/test/java/tests/TestSetting.java
@@ -6,15 +6,15 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
 public class TestSetting {
 
-    @BeforeAll
-    static void setup() {
+    @BeforeEach
+    void setup() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = System.getProperty("browserSize");
         Configuration.timeout = 10000;
